309b9a9442dff09d1ce9eb6a64101cfeb111901c
GSINET\peetersji <peetersji@delawareconsulting.com>
2017-01-18 13:33:44 +0100

    added toaster messages

--- app/components/messages/message-body.js
+++ app/components/messages/message-body.js
@@ -1,23 +1,47 @@
-import React, { PropTypes } from 'react';
-import styles from './message-body.css';
+import React, { Component, PropTypes } from 'react';
+import Alert from 'react-s-alert';
 
-function getCorrectClass(message) {
-  if (message.type === 'E') {
-    return styles.error;
+class MessageBody extends Component {
+
+  constructor() {
+    super();
+    this.checkErrorMessage = this.checkErrorMessage.bind(this);
   }
-  return styles.success;
-}
 
-const MessageBody = (props) => {
-  const { message } = props;
-  return (
-    <div>
-      <p className={getCorrectClass(message)}>
-        {message.message}
-      </p>
-    </div>
-  );
-};
+  componentDidMount() {
+    if (this.checkErrorMessage(this.props.message.type)) {
+      Alert.error(this.props.message.message, {
+        position: 'bottom-left',
+        effect: 'slide',
+        html: false,
+        offset: 50,
+        timeout: 5000,
+      });
+    } else {
+      Alert.warning(this.props.message.message, {
+        position: 'bottom-left',
+        effect: 'slide',
+        html: false,
+        offset: 50,
+        timeout: 5000,
+      });
+    }
+  }
+
+  checkErrorMessage(type) {
+    if (type === 'E') {
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
 
 MessageBody.propTypes = {
   message: PropTypes.object.isRequired,
--- app/containers/app-container.js
+++ app/containers/app-container.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+import 'bootstrap-grid/dist/grid.min.css';
 import MessagesContainer from './messages-container';
 import { getCheckMessages } from '../selectors/message-selectors';
-import 'bootstrap-grid/dist/grid.min.css';
 import './app-container.css';
 import Header from './header';
 import OverviewContainer from './overview-container';
 import PreviewContainer from './preview-container';
 import DropdownsContainer from './dropdowns-container';
 import OrderContainer from './order-container';
+import CustomToasterTemplate from '../components/messages/customToasterTemplate';
 
 class AppContainer extends Component {
   render() {
     return (
       <div className='container'>
+        <Alert stack={{ limit: false, spacing: 10 }} />
         <div className='row'>
           <div className='col-md-12'>
             <Header />
